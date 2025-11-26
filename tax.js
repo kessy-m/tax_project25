c1300161da6bbc6c3ddcce65bd3641effe5d2a07
kessy-m <kessymuriithi@gmail.com>
2025-11-26 15:55:59 +0300

    first tax project commit

---
+++ tax.js
@@ -0,0 +1,17 @@
+document.getElementById('tax_form').addEventListener('sumbit',function(event){
+event.preventDefault()
+
+    let basic_salary = Number(document.getElementById('basic').value)
+    let benefits = Number(document.getElementById('benefits').value)
+
+
+    function getGross(basic, benefits) {
+        return basic + benefits
+    }
+
+
+    let gross = getGross(basic_salary, benefits)
+    document.getElementById('gross').innerHTML = gross
+
+
+})
